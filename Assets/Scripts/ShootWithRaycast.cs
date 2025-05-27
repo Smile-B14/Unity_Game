f756f27e6882f320ecb0ba61ef95780c093e848e
Davit Agashenashvili <crazyblueboy3@gmail.com>
2025-05-27 19:57:15 +0400

    Raycast Bullet Spawn
    First Homework

--- Assets/Scripts/ShootWithRaycast.cs
+++ Assets/Scripts/ShootWithRaycast.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class ShootWithRaycast : MonoBehaviour
 {
     [SerializeField] private float raycastLength;
+    [SerializeField] private GameObject ShootPosition;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, raycastLength))
             {
-                Debug.Log(hit.point);
+                Instantiate(ShootPosition, hit.point, Quaternion.identity);
             }
         }
         Debug.DrawRay(transform.position, transform.forward * raycastLength, Color.red);
