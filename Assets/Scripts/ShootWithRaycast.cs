14a7fc21d0dd8bf5f38554d2d6543eca922239d6
Smile B <crazyblueboy3@gmail.com>
2025-05-30 05:18:40 -0700

    Update ShootWithRaycast.cs

--- Assets/Scripts/ShootWithRaycast.cs
+++ Assets/Scripts/ShootWithRaycast.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class ShootWithRaycast : MonoBehaviour
 {
     [SerializeField] private float raycastLength;
-    [SerializeField] private GameObject ShootPosition;
+    [SerializeField] private GameObject bulletPrefab;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, raycastLength))
             {
-                Instantiate(ShootPosition, hit.point, Quaternion.identity);
+                Instantiate(bulletPrefab, hit.point, Quaternion.identity);
             }
         }
         Debug.DrawRay(transform.position, transform.forward * raycastLength, Color.red);
